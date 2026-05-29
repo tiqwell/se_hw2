d25f1e54168a6e2c20f9653fa7bd9de58b34b70f
Тимофей Коптюбенко <tdkoptyubenko@edu.hse.ru>
2026-05-29 23:12:30 +0300

    Реализован класс Ingredient

---
+++ main.py
@@ -0,0 +1,29 @@
+class Ingredient:
+
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        value = float(value)
+        if value <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self._quantity = value
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
+
+    def __eq__(self, a):
+        if type(a) != type(self):
+            return False
+        return self.name == a.name and self.unit == a.unit
